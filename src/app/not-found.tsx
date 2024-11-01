3cadbe523e305b5cf0dcbc29856b1fc2225f9be1
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2024-11-01 11:53:11 +0800

    added common Not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import Box from '@mui/material/Box';
+
+const Home: React.FC = () => {
+  return (
+    <Box component="main" sx={{ display: 'flex', flex: '1 1 100%' }}>
+      Page not found
+    </Box>
+  );
+};
+
+export default Home;

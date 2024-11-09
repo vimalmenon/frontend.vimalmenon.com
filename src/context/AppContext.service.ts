945040652e4e5a30e13c264047903aa6aee0dd99
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2024-11-09 21:25:43 +0800

    added useThemeHelper

--- src/context/AppContext.service.ts
+++ src/context/AppContext.service.ts
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NotImplemented } from '@utility';
-import { IAppContext } from './AppContext';
+import { IAppContext, IUseThemeHelper } from './AppContext';
 
 export const initialValue: IAppContext = {
   isDark: true,
@@ -8,3 +8,18 @@ export const initialValue: IAppContext = {
 };
 
 export const AppContext = React.createContext<IAppContext>(initialValue);
+
+export const useAppContext = (): IAppContext => {
+  return React.useContext(AppContext);
+};
+
+export const useThemeHelper = (): IUseThemeHelper => {
+  const { isDark, setIsDark } = useAppContext();
+  const toggleTheme = (): void => {
+    setIsDark(!isDark);
+  };
+  return {
+    isDark,
+    toggleTheme,
+  };
+};

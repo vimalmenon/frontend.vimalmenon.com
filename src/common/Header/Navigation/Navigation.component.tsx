f968a1eb79da190ea394cf9cd63ede5322c2ae33
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2024-11-10 11:34:59 +0800

    working on Navigation

--- src/common/Header/Navigation/Navigation.component.tsx
+++ src/common/Header/Navigation/Navigation.component.tsx
@@ -4,20 +4,25 @@ import { usePathname } from 'next/navigation';
 import { NavigationData } from '@data';
 import Link from 'next/link';
 import Button from '@mui/material/Button';
-
+import Box from '@mui/material/Box';
 export const Navigation: React.FC = () => {
   const pathName = usePathname();
-  return NavigationData.map((nav) => {
-    return (
-      <Link
-        href={nav.url}
-        style={{ color: 'inherit', textDecoration: 'none', fontWeight: 'bold' }}
-        key={nav.name}
-      >
-        <Button color="inherit" variant={pathName === nav.url ? 'contained' : 'text'}>
-          {nav.name}
-        </Button>
-      </Link>
-    );
-  });
+
+  return (
+    <Box sx={{ display: 'flex', gap: 1 }}>
+      {NavigationData.map((nav) => {
+        return (
+          <Link
+            href={nav.url}
+            style={{ color: 'inherit', textDecoration: 'none', fontWeight: 'bold' }}
+            key={nav.name}
+          >
+            <Button color="inherit" variant={pathName === nav.url ? 'contained' : 'text'}>
+              {nav.name}
+            </Button>
+          </Link>
+        );
+      })}
+    </Box>
+  );
 };

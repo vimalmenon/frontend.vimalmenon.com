bfd91dc7461a4198941d74d47ee4bc0643967964
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2024-11-25 21:58:33 +0800

    Updated more NeoVim Command

--- src/data/neoVimCommands.ts
+++ src/data/neoVimCommands.ts
@@ -5,6 +5,15 @@ export const NeoVimCommands: ICommand[] = [
     describe: 'Show Buffers in telescope',
     command: 'space space',
     language: 'bash',
-
+  },
+  {
+    describe: 'Show navigation tree',
+    command: '\\',
+    language: 'bash',
+  },
+  {
+    describe: 'Refresh the buffer',
+    command: 'F5',
+    language: 'bash',
   },
 ];

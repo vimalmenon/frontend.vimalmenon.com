0dff1127b9f31ceec2ccbef3f43806236e8cd35d
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2024-11-25 21:41:51 +0800

    Updated NeoVim command

--- src/data/neoVimCommands.ts
+++ src/data/neoVimCommands.ts
@@ -1,3 +1,10 @@
 import { ICommand } from '@types';
 
-export const NeoVimCommands: ICommand[] = [];
+export const NeoVimCommands: ICommand[] = [
+  {
+    describe: 'Show Buffers in telescope',
+    command: 'space space',
+    language: 'bash',
+
+  },
+];

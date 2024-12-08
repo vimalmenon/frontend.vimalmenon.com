64ee65cb5628bf4afc52c4ad7c04801c8b013ed6
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2024-12-09 02:17:22 +0800

    updated env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Box from '@mui/material/Box';
 
 import { env } from '../config';
+
+
 const Home: React.FC = () => {
-  return <Box>This is Home page {env.API} </Box>;
+  return <Box>This is Home page {env.API} {JSON.stringify(env)} </Box>;
 };
 
 export default Home;

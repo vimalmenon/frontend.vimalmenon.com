9492ff403a33b5a3871a9bfecad0d65a06ba7da1
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2024-12-08 14:46:20 +0800

    fixed eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Box from '@mui/material/Box';
 
-import { env } from "../config"
+import { env } from '../config';
 const Home: React.FC = () => {
   return <Box>This is Home page {env.API} </Box>;
 };

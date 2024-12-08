3cdd3ed111785dd1784b3aeb43d9bd205472a894
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2024-12-08 13:35:58 +0800

    updated Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import Box from '@mui/material/Box';
 
+import { env } from "../config"
 const Home: React.FC = () => {
-  return <Box>This is Home page</Box>;
+  return <Box>This is Home page {env.API} </Box>;
 };
 
 export default Home;

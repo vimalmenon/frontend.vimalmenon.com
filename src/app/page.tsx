3ac7905c5a8f7b78130c5226450886c618054346
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2024-12-11 04:56:16 +0800

    updated Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,20 @@ import { env } from '../config';
 export const dynamic = 'force-dynamic';
 
 const Home: React.FC = async () => {
-  return <Box>This is Home page {env.API}</Box>;
+  return (
+    <Box>
+      This is Home page
+      <Box>
+        <Box>API</Box>
+        <Box>{env.API}</Box>
+      </Box>
+      <Box>
+        <Box>Env</Box>
+        <Box>{env.ENV}</Box>
+      </Box>
+
+    </Box>
+  );
 };
 
 export default Home;

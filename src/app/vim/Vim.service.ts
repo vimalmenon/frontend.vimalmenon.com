2380aa46928a32945e8164f1ad83e81ff118a57d
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2024-12-17 21:10:55 +0800

    updated post

--- src/app/vim/Vim.service.ts
+++ src/app/vim/Vim.service.ts
@@ -1,8 +1,8 @@
 import React from 'react';
 import { IVimContext } from './Vim';
-import { NotImplemented } from '@utility';
+import { NotImplemented, apiCaller } from '@utility';
 import { ICommand } from '@types';
-import { env } from '@constants';
+import { API } from '@constants';
 
 export const VimContext = React.createContext<IVimContext>({
   mode: 'VIEW',
@@ -15,15 +15,8 @@ export const useVimContext = (): IVimContext => {
 
 export const useVimForm = () => {
   const { mode, setMode } = useVimContext();
-  const onFormSave = (data: ICommand): void => {
-    fetch(`${env.API}/vim/`, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    });
+  const onFormSave = async (data: ICommand): Promise<void> => {
+    await apiCaller(API.PostVimData(data));
   };
   const onFormToggle = (): void => {
     setMode(mode === 'VIEW' ? 'EDIT' : 'VIEW');
--- src/utility/apiCaller.ts
+++ src/utility/apiCaller.ts
@@ -4,6 +4,10 @@ export const apiCaller = async <T>(data: IApi): Promise<T> => {
   const response = await fetch(data.url, {
     method: data.method,
     body: data.body,
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
   });
   return await response.json();
 };

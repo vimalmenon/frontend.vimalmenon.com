c9a980e730e8a2b324311126ecc0034fbccf728c
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2024-12-20 06:32:04 +0800

    show loading indicator

---
+++ src/app/vim/loading.tsx
@@ -0,0 +1,16 @@
+import Stack from '@mui/material/Stack';
+import Skeleton from '@mui/material/Skeleton';
+import type { NextPage } from 'next';
+
+const Loading: NextPage = () => {
+  return (
+    <Stack spacing={1}>
+      <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+      <Skeleton variant="circular" width={40} height={40} />
+      <Skeleton variant="rectangular" width={210} height={60} />
+      <Skeleton variant="rounded" width={210} height={60} />
+    </Stack>
+  );
+};
+
+export default Loading;

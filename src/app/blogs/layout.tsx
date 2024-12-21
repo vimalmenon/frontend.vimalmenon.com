a9501868d24fdc2d2ed7bac3e3d191ed3ae8095d
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2024-12-21 18:37:44 +0800

    updated layout

--- src/app/blogs/layout.tsx
+++ src/app/blogs/layout.tsx
@@ -1,9 +1,4 @@
 import React from 'react';
-import { AppContext } from '@context';
-import { Body, Footer, Header } from '@common';
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
-import Container from '@mui/material/Container';
-import CssBaseline from '@mui/material/CssBaseline';
 import type { Metadata } from 'next';
 import Box from '@mui/material/Box';
 import { IReactChildren } from '@types';
@@ -15,28 +10,10 @@ export const metadata: Metadata = {
 
 const BlogsLayout: React.FC<IReactChildren> = ({ children }) => {
   return (
-    <html lang="en">
-      <meta name="viewport" content="initial-scale=2, width=device-width" />
-      <body>
-        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-          <AppContext>
-            <CssBaseline />
-            <Container
-              maxWidth={false}
-              disableGutters
-              sx={{ display: 'flex', flexDirection: 'column', height: '101vh' }}
-            >
-              <Header />
-              <Body>
-                <div>this is vimal menon</div>
-                <Box sx={{ padding: 3, flex: '1 1 100%' }}>{children}</Box>
-              </Body>
-              <Footer />
-            </Container>
-          </AppContext>
-        </AppRouterCacheProvider>
-      </body>
-    </html>
+    <Box sx={{ padding: 3, flex: '1 1 100%' }}>
+      <div>this is vimal menon</div>
+      {children}
+    </Box>
   );
 };
 

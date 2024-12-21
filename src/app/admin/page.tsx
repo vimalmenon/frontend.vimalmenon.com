98e1393147876f11ab83a24a30fd07b0c9e37dea
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2024-12-21 18:06:17 +0800

    Updated Admin blogs

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,17 +1,18 @@
+
 import type { Metadata, NextPage } from 'next';
 import Box from '@mui/material/Box';
 
 export const metadata: Metadata = {
-  title: 'Blogs | Vimal Menon',
+  title: 'Admin | Vimal Menon',
   description: 'This is my personal website',
 };
 
-const Blogs: NextPage = async () => {
+const Admin: NextPage = async () => {
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-      <Box>This is Blogs page</Box>
+      <Box>This is Admin page</Box>
     </Box>
   );
 };
 
-export default Blogs;
+export default Admin;

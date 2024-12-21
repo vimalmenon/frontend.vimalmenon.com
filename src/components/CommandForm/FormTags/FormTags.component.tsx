fd816de254e0b8549ddc881d6e54f45c6baa4bad
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2024-12-21 12:13:15 +0800

    updated form tags

--- src/components/CommandForm/FormTags/FormTags.component.tsx
+++ src/components/CommandForm/FormTags/FormTags.component.tsx
@@ -1,12 +1,34 @@
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import { IFormTags } from './FormTags';
+import { useState } from 'react';
+import Chip from '@mui/material/Chip';
 
 export const FormTags: React.FC<IFormTags> = ({ value }) => {
+  const [tags, setTags] = useState<string[]>(value);
+  const [inputTag, setInputTag] = useState<string>('');
+  const onEnter = (event: any): void => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      setTags([...tags, inputTag]);
+      setInputTag('');
+    }
+  };
   return (
-    <Box>
-      {value.join(', ')}
-      <TextField />
+    <Box sx={{ display: 'flex', gap: 2, flexDirection: 'column' }}>
+      <TextField
+        size="small"
+        value={inputTag}
+        onChange={(e) => setInputTag(e.target.value)}
+        onKeyDown={onEnter}
+        fullWidth
+        label="Tags"
+      />
+      <Box sx={{ display: 'flex', gap: 2 }}>
+        {tags.map((tag) => {
+          return <Chip label={tag} key={tag} />;
+        })}
+      </Box>
     </Box>
   );
 };

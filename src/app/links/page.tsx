af421dcb31dfd0f059e6e8a0c78fa57977b2a61e
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2024-12-24 06:46:57 +0800

    get links from data

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -1,32 +1,23 @@
 import type { Metadata, NextPage } from 'next';
+import { links } from '@data';
 
 export const metadata: Metadata = {
-  title: 'Links Tutorial | Vimal Menon',
+  title: 'Links | Vimal Menon',
   description: 'This is my personal website',
 };
 
 const LinksPage: NextPage = () => {
   return (
     <div>
-      <div>
-        <a href="https://crontab.guru/" target="blank">
-          Cron Tab
-        </a>
-      </div>
-      <div>
-        <a href="https://github.com/vimalmenon/neovim" target="_blank" rel="noreferrer">
-          GitHub
-        </a>
-      </div>
-      <div>
-        <a
-          href="https://www.reddit.com/r/neovim/comments/1bojtr0/please_share_your_nvimcmp_config/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Redis Nvim Config
-        </a>
-      </div>
+      {links.map((link) => {
+        return (
+          <div key={link.name}>
+            <a href={link.link} target="_blank" rel="noreferrer">
+              {link.name}
+            </a>
+          </div>
+        );
+      })}
     </div>
   );
 };

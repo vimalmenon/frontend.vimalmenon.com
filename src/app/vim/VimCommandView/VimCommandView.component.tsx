00847e96ae098be5b8134586013b9a95ca983e3c
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2024-12-26 06:45:47 +0800

    updated vim command

--- src/app/vim/VimCommandView/VimCommandView.component.tsx
+++ src/app/vim/VimCommandView/VimCommandView.component.tsx
@@ -7,6 +7,7 @@ import { Highlight } from '@components';
 import { IVimCommandView } from './VimCommandView';
 import { Icons } from '@constants';
 import IconButton from '@mui/material/IconButton';
+import Chip from '@mui/material/Chip';
 
 export const VimCommandView: React.FC<IVimCommandView> = ({ commands }) => {
   const { mode, onCommandDelete, onFormEdit } = useVimForm();
@@ -21,17 +22,24 @@ export const VimCommandView: React.FC<IVimCommandView> = ({ commands }) => {
               <Fragment key={index}>
                 <Box sx={{ margin: 0.5, display: 'flex', justifyContent: 'space-between' }}>
                   {data.describe}
-                  <div>{data.tags.join(', ')}</div>
-                  {data.id ? (
-                    <div>
-                      <IconButton size="small" onClick={() => onFormEdit(data)}>
-                        <Icons.Edit />
-                      </IconButton>
-                      <IconButton size="small" onClick={() => onCommandDelete(data.id)}>
-                        <Icons.Delete />
-                      </IconButton>
-                    </div>
-                  ) : null}
+                  <Box sx={{ display: 'flex', gap: 4 }}>
+                    <Box sx={{ gap: 1, display: 'flex' }}>
+                      {data.tags.map((tag) => {
+                        return <Chip label={tag} key={tag} />;
+                      })}
+                    </Box>
+
+                    {data.id ? (
+                      <div>
+                        <IconButton size="small" onClick={() => onFormEdit(data)}>
+                          <Icons.Edit />
+                        </IconButton>
+                        <IconButton size="small" onClick={() => onCommandDelete(data.id)}>
+                          <Icons.Delete />
+                        </IconButton>
+                      </div>
+                    ) : null}
+                  </Box>
                 </Box>
                 <Highlight code={data.command} language={data.language} />
               </Fragment>

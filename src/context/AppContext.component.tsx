a13cb8055666ed784a52d80864624dec56aa2d82
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2024-12-25 22:07:47 +0800

    updated package.json

--- src/context/AppContext.component.tsx
+++ src/context/AppContext.component.tsx
@@ -1,24 +1,30 @@
 'use client';
 
 import React, { useState } from 'react';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { IReactChildren } from '@types';
 import { ThemeProvider } from '@mui/material/styles';
 import { generateTheme } from '@utility';
 
 import { AppContext as Context } from './AppContext.service';
 
+const queryClient = new QueryClient();
+
 export const AppContext: React.FC<IReactChildren> = ({ children }) => {
   const [isDark, setIsDark] = useState<boolean>(false);
   const theme = generateTheme(!isDark);
   return (
-    <Context.Provider
-      value={{
-        isDark,
-        setIsDark,
-      }}
-    >
-      <ThemeProvider theme={theme}>{children}</ThemeProvider>
-    </Context.Provider>
+    <QueryClientProvider client={queryClient}>
+      <Context.Provider
+        value={{
+          isDark,
+          setIsDark,
+        }}
+      >
+        <ThemeProvider theme={theme}>{children}</ThemeProvider>
+      </Context.Provider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };

91f7213d788184ba807a018eca901c58db17b75e
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2024-12-28 08:32:43 +0800

    updated Blogs page

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -3,6 +3,9 @@ import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardHeader from '@mui/material/CardHeader';
+import CardActions from '@mui/material/CardActions';
+import NextLink from 'next/link';
+import Button from '@mui/material/Button';
 
 export const metadata: Metadata = {
   title: 'Blogs | Vimal Menon',
@@ -15,14 +18,29 @@ const Blogs: NextPage = async () => {
       <Card>
         <CardHeader title="VIM / NVIM" subheader="Learn all about Vim navigation" />
         <CardContent>This is card content</CardContent>
+        <CardActions>
+          <Button LinkComponent={NextLink} href="/blogs/vim">
+            Vim
+          </Button>
+        </CardActions>
       </Card>
       <Card>
         <CardHeader title="Linux Command" subheader="Learn all about Linux Command" />
         <CardContent>This is card content</CardContent>
+        <CardActions>
+          <Button LinkComponent={NextLink} href="/blogs/linux">
+            Linux
+          </Button>
+        </CardActions>
       </Card>
       <Card>
         <CardHeader title="Rust" subheader="Learn all about Rust programming language" />
         <CardContent>This is card content</CardContent>
+        <CardActions>
+          <Button LinkComponent={NextLink} href="/blogs/rust">
+            Rust
+          </Button>
+        </CardActions>
       </Card>
     </Box>
   );

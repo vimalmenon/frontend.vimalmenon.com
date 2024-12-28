3179842a0e21023bb8e7002f6e386d3dc5d68767
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2024-12-28 21:48:45 +0800

    Vim Delete Confirm

--- src/app/blogs/vim/VimDeleteConfirmation/VimDeleteConfirmation.component.tsx
+++ src/app/blogs/vim/VimDeleteConfirmation/VimDeleteConfirmation.component.tsx
@@ -5,16 +5,20 @@ import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogTitle from '@mui/material/DialogTitle';
 import Button from '@mui/material/Button';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
 
 export const VimDeleteConfirmation: React.FC = () => {
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down('lg'));
   const { mode } = useVimDeleteHelper();
-  if (mode === 'DELETE') {
+  if (mode === 'VIEW') {
     return (
-      <Dialog open={true}>
+      <Dialog open={true} fullScreen={fullScreen}>
         <DialogTitle>Are you sure you want to Delete?</DialogTitle>
         <DialogActions>
-          <Button autoFocus>Disagree</Button>
-          <Button autoFocus>Agree</Button>
+          <Button autoFocus>Cancel</Button>
+          <Button autoFocus>Confirm</Button>
         </DialogActions>
       </Dialog>
     );

277c9568edd10ef69706ff726edbd4aadcf51038
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2024-12-28 14:19:03 +0800

    updated page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,7 @@
 import type { Metadata, NextPage } from 'next';
 import Box from '@mui/material/Box';
+import { items } from './admin.data';
+import { NavigationItem } from '@components';
 
 export const metadata: Metadata = {
   title: 'Admin | Vimal Menon',
@@ -10,6 +12,9 @@ const Admin: NextPage = async () => {
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column' }}>
       <Box>This is Admin page</Box>
+      {items.map((data) => {
+        return <NavigationItem data={data} key={data.title} />;
+      })}
     </Box>
   );
 };

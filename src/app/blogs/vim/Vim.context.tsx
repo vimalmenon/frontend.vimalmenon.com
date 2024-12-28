943dcb2ed90657c72dc09166b505da35d335dc04
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2024-12-28 15:14:54 +0800

    updated Vim Command

--- src/app/blogs/vim/Vim.context.tsx
+++ src/app/blogs/vim/Vim.context.tsx
@@ -5,13 +5,18 @@ import { useEffect, useState } from 'react';
 
 import { IContext } from './Vim';
 import { VimContext as Context } from './Vim.service';
+import { useQuery } from '@tanstack/react-query';
 
 export const VimContext: React.FC<IContext> = ({ children, commands }) => {
   const [mode, setMode] = useState<FormMode>('VIEW');
   const [command, setCommand] = useState<ICommand | undefined>();
   const [search, setSearch] = useState<string>('');
   const [selectedTags, setSelectedTags] = useState<ICommandTag[]>([]);
-
+  useQuery<ICommand[]>({
+    queryKey: ["Command"],
+    queryFn: () => Promise.resolve([]),
+    initialData: commands
+  })
   useEffect(() => {
     const tags = commands.reduce<string[]>((initialValue, command) => {
       const tags = command.tags.filter((tag) => {

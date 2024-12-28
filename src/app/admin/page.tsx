c8344cb09cfe0abc9ef43fff6e4006d9a0f36b30
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2024-12-28 14:19:43 +0800

    updated Page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -10,8 +10,7 @@ export const metadata: Metadata = {
 
 const Admin: NextPage = async () => {
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-      <Box>This is Admin page</Box>
+    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
       {items.map((data) => {
         return <NavigationItem data={data} key={data.title} />;
       })}

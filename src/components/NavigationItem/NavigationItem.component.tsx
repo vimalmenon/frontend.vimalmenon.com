d4eb1b8017e523bf809c48cbaef2f4a922960f94
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-01-01 12:05:40 +0800

    Added Naivation Item

--- src/components/NavigationItem/NavigationItem.component.tsx
+++ src/components/NavigationItem/NavigationItem.component.tsx
@@ -2,9 +2,9 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardHeader from '@mui/material/CardHeader';
 import NextLink from 'next/link';
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import { INavigationItem } from './NavigationItem';
 import { IconButton } from '@mui/material';
+import { Icons } from '@constants';
 
 export const NavigationItem: React.FC<INavigationItem> = ({ data }) => {
   return (
@@ -13,7 +13,7 @@ export const NavigationItem: React.FC<INavigationItem> = ({ data }) => {
         title={data.title}
         action={
           <IconButton LinkComponent={NextLink} href={data.link}>
-            <KeyboardArrowRightIcon />
+            <Icons.KeyboardArrowRight />
           </IconButton>
         }
       />
--- src/constants/icons.ts
+++ src/constants/icons.ts
@@ -3,6 +3,7 @@ import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import SaveIcon from '@mui/icons-material/Save';
 import CloseIcon from '@mui/icons-material/Close';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 export const Icons = {
   Add: AddIcon,
@@ -10,4 +11,5 @@ export const Icons = {
   Delete: DeleteIcon,
   Save: SaveIcon,
   Close: CloseIcon,
+  KeyboardArrowRight: KeyboardArrowRightIcon,
 };

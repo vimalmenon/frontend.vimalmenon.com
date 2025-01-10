1d17cbc5a696eb7cf9e12a531f7a02007b84aaaa
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-01-10 10:35:45 +0800

    updated Vim Panel

--- src/app/admin/vim/VimPanel/VimPanel.component.tsx
+++ src/app/admin/vim/VimPanel/VimPanel.component.tsx
@@ -2,11 +2,13 @@
 
 import { Alert } from '@components';
 import Box from '@mui/material/Box';
+import { useAlertHelper } from '../Vim.service';
 
 export const VimPanel: React.FC = () => {
+  const { onAlertClose } = useAlertHelper();
   return (
     <Box>
-      <Alert alert={undefined} />
+      <Alert alert={undefined} onClose={onAlertClose} />
     </Box>
   );
 };

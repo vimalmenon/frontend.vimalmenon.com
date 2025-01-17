a6965587313498ac91c4cc8b1f3857a2fb372d7b
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-01-17 12:22:05 +0800

    updated Naivation data

--- src/data/navigationData.ts
+++ src/data/navigationData.ts
@@ -5,6 +5,7 @@ class Navigation implements INavigation {
     public name: string,
     public url: string,
     public title: string,
+    public description: string,
     public breadcrumbs: INavigation[]
   ) {
     this.breadcrumbs = [
@@ -19,31 +20,81 @@ class Navigation implements INavigation {
 }
 
 //Home Navigation
-const Home = new Navigation('Home', '/', 'Home', []);
-const Admin = new Navigation('Admin', '/admin', 'Admin | Home', [Home]);
-const Blogs = new Navigation('Blogs', '/blogs', 'Blogs | Home', [Home]);
+const Home = new Navigation('Home', '/', 'Home', 'This is Home Page for Vimal Menon', []);
+const Admin = new Navigation(
+  'Admin',
+  '/admin',
+  'Admin | Home',
+  'This is Home Page for Vimal Menon',
+  [Home]
+);
+const Blogs = new Navigation(
+  'Blogs',
+  '/blogs',
+  'Blogs | Home',
+  'This is Home Page for Vimal Menon',
+  [Home]
+);
 
 //Admin Navigation
-const AdminLinks = new Navigation('Links', '/admin/links', 'Links | Admin | Home', [Home, Admin]);
-const AdminVim = new Navigation('VIM', '/admin/vim', 'Vim | Admin | Home', [Home, Admin]);
-const AdminLinux = new Navigation('Linux', '/admin/linux', 'Linux | Admin | Home', [Home, Admin]);
+const AdminLinks = new Navigation(
+  'Links',
+  '/admin/links',
+  'Links | Admin | Home',
+  'This is Home Page for Vimal Menon',
+  [Home, Admin]
+);
+const AdminVim = new Navigation(
+  'VIM',
+  '/admin/vim',
+  'Vim | Admin | Home',
+  'This is Home Page for Vimal Menon',
+  [Home, Admin]
+);
+const AdminLinux = new Navigation(
+  'Linux',
+  '/admin/linux',
+  'Linux | Admin | Home',
+  'This is Home Page for Vimal Menon',
+  [Home, Admin]
+);
 
 //Blogs Navigation
-const Linux = new Navigation('Linux', '/blogs/linux', 'Linux | Blogs | Home', [Home, Blogs]);
-const Rust = new Navigation('Rust', '/blogs/rust', 'Rust | Blogs | Home', [Home, Blogs]);
-const Vim = new Navigation('VIM', '/blogs/vim', 'Vim | Blogs | Home', [Home, Blogs]);
+const Linux = new Navigation(
+  'Linux',
+  '/blogs/linux',
+  'Linux | Blogs | Home',
+  'This is Home Page for Vimal Menon',
+  [Home, Blogs]
+);
+const Rust = new Navigation(
+  'Rust',
+  '/blogs/rust',
+  'Rust | Blogs | Home',
+  'This is Home Page for Vimal Menon',
+  [Home, Blogs]
+);
+const Vim = new Navigation(
+  'VIM',
+  '/blogs/vim',
+  'Vim | Blogs | Home',
+  'This is Home Page for Vimal Menon',
+  [Home, Blogs]
+);
 
 // Vim Navigation
 const VimInstall = new Navigation(
   'Install NVIM',
   'Install NVIM | Vim | Blogs | Home',
   `${Vim.url}/install-nvim`,
+  'This is Home Page for Vimal Menon',
   [Home, Blogs, Vim]
 );
 const VimNavigation = new Navigation(
   'Basic Navigation',
   'Basic Navigation | Vim | Blogs | Home',
   `${Vim.url}/basic-navigation`,
+  'This is Home Page for Vimal Menon',
   [Home, Blogs, Vim]
 );
 

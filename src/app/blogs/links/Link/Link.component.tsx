e572650bd8cd7cc7fc360b0e61a3e884dfb94c7a
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-01-19 19:56:48 +0800

    updated link

--- src/app/blogs/links/Link/Link.component.tsx
+++ src/app/blogs/links/Link/Link.component.tsx
@@ -4,25 +4,29 @@ import Box from '@mui/material/Box';
 import { ILinkComponent } from './Link';
 
 import MuiLink from '@mui/material/Link';
+import { useState } from 'react';
 
 export const Link: React.FC<ILinkComponent> = ({ data, index }) => {
+  const [show, setShow] = useState<boolean>(false);
   return (
     <Box sx={{ margin: 0.5 }}>
-      <Box>
+      <Box onClick={() => setShow(!show)}>
         [{index + 1}] {data.name}
       </Box>
-      <Box>
-        {data.links.map((link, index) => {
-          return (
-            <Box key={index} sx={{ marginLeft: 2, display: 'flex', gap: 1 }}>
-              ({index + 1})
-              <MuiLink href={link.link} target="_blank" underline="none">
-                {link.name}
-              </MuiLink>
-            </Box>
-          );
-        })}
-      </Box>
+      {show ? (
+        <Box>
+          {data.links.map((link, index) => {
+            return (
+              <Box key={index} sx={{ marginLeft: 2, display: 'flex', gap: 1 }}>
+                ({index + 1})
+                <MuiLink href={link.link} target="_blank" underline="none">
+                  {link.name}
+                </MuiLink>
+              </Box>
+            );
+          })}
+        </Box>
+      ) : null}
     </Box>
   );
 };

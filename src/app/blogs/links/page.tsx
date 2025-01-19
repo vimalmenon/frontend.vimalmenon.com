95167dd5cc559a82fd7db9b20adff8bd33c227cb
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2025-01-19 10:38:20 +0800

    updated link page

--- src/app/blogs/links/page.tsx
+++ src/app/blogs/links/page.tsx
@@ -1,7 +1,5 @@
 import Box from '@mui/material/Box';
-import { linuxCommands } from '@data';
-import { Fragment } from 'react';
-import { Highlight } from '@components';
+import { links } from '@data';
 import type { Metadata, NextPage } from 'next';
 
 export const metadata: Metadata = {
@@ -12,14 +10,16 @@ export const metadata: Metadata = {
 const Page: NextPage = () => {
   return (
     <Box>
-      {linuxCommands.map((data, index) => {
+      {links.map((data, index) => {
         return (
-          <Fragment key={index}>
-            <Box component="p" sx={{ margin: 0.5 }}>
-              {data.describe}
+          <Box component="p" sx={{ margin: 0.5 }} key={index}>
+            <Box>{data.name}</Box>
+            <Box>
+              {data.links.map((link, index) => {
+                return <Box key={index}>{link.name}</Box>;
+              })}
             </Box>
-            <Highlight code={data.command} language={data.language} />
-          </Fragment>
+          </Box>
         );
       })}
     </Box>
